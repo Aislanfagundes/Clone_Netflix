5730da854a2daa6f8849a4af49ce20b78ca51002
Aislan <aislan.fagundes@hotmail.com>
2022-07-07 09:27:21 -0300

    adicionado na pagina filmes/ a lista de filmes novos e em alta

---
+++ filme/novos_context.py
@@ -0,0 +1,9 @@
+from .models import Filme
+
+def lista_filmes_recentes(request):
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')
+    return {'lista_filmes_recentes': lista_filmes}
+
+def lista_filmes_emalta(request):
+    lista_filmes = Filme.objects.all().order_by('-visualizacao')
+    return {'lista_filmes_emalta': lista_filmes} # é essa variação que vamos usar no HTML, então não pode ser um nome já usado
