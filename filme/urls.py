58c600499cc5b5f2eac4d56f1d95f2c69685be4b
Aislan <aislan.fagundes@hotmail.com>
2022-07-25 20:48:56 -0300

    Configuração da barra de pesquisa no navbar e pagina de pesquisa no url

--- filme/urls.py
+++ filme/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 
 from django.urls import path, include
-from .views import Homefilmes, Homepage, Detalhesfilme
+from .views import Homefilmes, Homepage, Detalhesfilme, Pesquisafilme
 
 app_name = 'filme'
 
 urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('filmes/', Homefilmes.as_view(), name='homefilmes'),
-    path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme')
+    path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme'),
+    path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
 
 
 ]
