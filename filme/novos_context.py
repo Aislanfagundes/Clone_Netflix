ff8fe7ae6400fab4ce62327ede01df7ca8add07b
Aislan <aislan.fagundes@hotmail.com>
2022-07-25 20:08:03 -0300

    Subindo alguns ajustes nas paginas homefilmes e home pages, linkando os videos e colocando um filme como 'principal'

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -7,3 +7,7 @@ def lista_filmes_recentes(request):
 def lista_filmes_emalta(request):
     lista_filmes = Filme.objects.all().order_by('-visualizacao')
     return {'lista_filmes_emalta': lista_filmes} # é essa variação que vamos usar no HTML, então não pode ser um nome já usado
+
+def filme_destaque(request):
+    filme = Filme.objects.order_by('-data_criacao')[0]
+    return {"filme_destaque": filme}
